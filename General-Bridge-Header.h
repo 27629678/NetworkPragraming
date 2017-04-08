863ff6fd079feaa65363c76d7f5d28b96709d1f8
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2017-04-08 11:26:23 +0800

    fix issue:fis unit testing bug

---
+++ General-Bridge-Header.h
@@ -0,0 +1,17 @@
+//
+//  General-Bridge-Header.h
+//  NetworkPragramming
+//
+//  Created by hzyuxiaohua on 2017/4/8.
+//  Copyright © 2017年 hzyuxiaohua. All rights reserved.
+//
+
+#ifndef General_Bridge_Header_h
+#define General_Bridge_Header_h
+
+#import "AddrInfo.h"
+#import "ByteOrder.h"
+#import "BSDSocketServer.h"
+#import "NetworkInterfaceUtil.h"
+
+#endif /* General_Bridge_Header_h */
